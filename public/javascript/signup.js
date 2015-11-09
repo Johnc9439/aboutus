fe1163fca94661e2e65e5fd9757e3be3c97353c7
abbychang <abbychang551@gmail.com>
2015-11-09 15:25:16 +0800

    add

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     e.preventDefault();  
     var user = {
       username: $("#username").val(),
-      email: $("#email").val()  
+      passw: $("#password").val()  
     };
     console.log(user);
     $.ajax({
@@ -21,4 +21,51 @@ $(document).ready(function(){
       //  $('#signup').append(greeting);  
       //  };         
   });        
+});
+
+
+
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
 });
