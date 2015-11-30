d2fe503a32dc96373de9f4ec021b0b450d863300
Johnc9439 <john1234chon@yahoo.com.hk>
2015-11-30 19:43:10 +0800

    update

--- app.js
+++ app.js
@@ -1,75 +1,122 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var favicon = require('path');
 var fs = require('fs');
-//var filePath = 'text';
-//var info = JSON.parse(fs.readFileSync(filePath));
-app.use(express.static('public'));
-//app.get('/',function(req,res){
-//	res.sendFile(path.join(__dirname+'/index.html'));
-//});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-
-
-
-app.use(bodyParser.urlencoded({extended:false}));
-
-
-app.post('/Signup', function(req, res) {
-  user = {
-    username: req.body.username,
-    email: req.body.email
-  };
-  info.push(user);
-  fs.writeFileSync(filePath, JSON.stringify(info));
+//connect to mongoDB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://team19:secretsecret@localhost/team19');
+module.exports = mongoose.model('User', {
+	id: String,
+	username: String,
+	password: String,
+	email: String,
+	Nickname: String,
 });
 
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(looger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//configuring passport
+var passport = require('passport');
+var expressSession = require('express-session');
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var flash = require('connect-flash');
+app.use(flash());
+
+var initPassport = require('./passport/init');
+initPassport(passport);
+
+var routes =require('./routes/index')(passport);
+app.use('/', routes);
+
+app.use(function(req, res, next){
+	var err = new Error('Not Found');
+	err.status =404;
+	next(err);
+});
+
+if(app.get('env') === 'development'){
+	app.use(function(err, req, res, next){
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+app.listen(2314);
+console.log('Sever run');
+
+module.export =app;
 
 
-app.get('/', function (req, res) {
+
+
+// app.post('/Signup', function(req, res) {
+  // user = {
+    // username: req.body.username,
+    // email: req.body.email
+  // };
+  // info.push(user);
+  // fs.writeFileSync(filePath, JSON.stringify(info));
+// });
+
+
+
+// app.get('/', function (req, res) {
  
-  console.log('gogosister');
-  res.sendFile(path.join(__dirname+'/public/html/index.html'));
-  });
+  // console.log('gogosister');
+  // res.sendFile(path.join(__dirname+'/public/html/index.html'));
+  // });
 
 
 
 
 
-app.get('/journal', function (req, res) {
-  res.sendFile(path.join(__dirname+'/public/html/journal.html'));
-  });
+// app.get('/journal', function (req, res) {
+  // res.sendFile(path.join(__dirname+'/public/html/journal.html'));
+  // });
 
 
 
-app.get('/aboutus', function (req, res) {
-  res.sendFile(path.join(__dirname+'/public/html/aboutus.html'));
-  });
+// app.get('/aboutus', function (req, res) {
+  // res.sendFile(path.join(__dirname+'/public/html/aboutus.html'));
+  // });
 
 //app.get('/contact', function (req, res) {
-//  res.sendFile(path.join(__dirname+'/public/html/contact.html'));
-//  });
+ //res.sendFile(path.join(__dirname+'/public/html/contact.html'));
+ //});
 
-app.get('/signup', function (req, res) {
-  res.sendFile(path.join(__dirname+'/public/html/signup.html'));
-  });
-
-
-app.get('/register', function (req, res) {
-  res.sendFile(path.join(__dirname+'/public/html/register.html'));
-  });
+// app.get('/signup', function (req, res) {
+  // res.sendFile(path.join(__dirname+'/public/html/signup.html'));
+  // });
 
 
+// app.get('/register', function (req, res) {
+  // res.sendFile(path.join(__dirname+'/public/html/register.html'));
+  // });
 
 
 
-app.listen(8171);
-console.log('Server run');
+
+
+// app.listen(8171);
+// console.log('Server run');
 
 
 
