14432d598dcaa66de805014817f53ef5cef97802
Johnc9439 <john1234chon@yahoo.com.hk>
2015-11-30 20:27:39 +0800

    update2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(favicon());
-app.use(looger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(cookieParser());
